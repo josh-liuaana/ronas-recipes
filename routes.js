c36ad57377fbe6484963331720018c898e3cb53c
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-03-31 20:39:57 +1300

    create template routes

--- routes.js
+++ routes.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.send('<h1>HOMEPAGE IS A WORK IN PROGRESS</h1>')
 })
 
+router.get('/ingredients', (req, res) => {
+  res.send('THIS WILL SOON BE A LIST OF INGREDIENTS IN YOUR PANTRY')
+})
+
 module.exports = router
