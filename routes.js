be01ba01077176da2fd01f60c4333f6c136f4723
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-04-03 10:47:11 +1200

    update hbs redirection

--- routes.js
+++ routes.js
@@ -4,13 +4,13 @@ const db = require('./db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('<h1>HOMEPAGE IS A WORK IN PROGRESS</h1>')
+  res.render('home')
 })
 
 router.get('/ingredients', (req, res) => {
   db.getIngredients()
     .then((ingredients) => {
-      res.render('home', { ingredients: ingredients })
+      res.render('ingredients', { ingredients: ingredients })
       return null
     })
     .catch((err) => {
